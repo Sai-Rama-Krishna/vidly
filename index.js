5395f861430209d77177e4b102d3c4d567445c9b
sai rama krishna <sairamakrishna2@gmail.com>
2021-10-18 17:05:24 +0530

    sasasa

--- index.js
+++ index.js
@@ -12,4 +12,7 @@ require("./startup/validation")();
 require("./startup/prod")(app);
 
 const port = process.env.PORT || 3005;
-app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
+module.exports = server;
