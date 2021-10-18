88de17ae400ba99051cf1b3f99990e34664aca95
sai rama krishna <sairamakrishna2@gmail.com>
2021-10-18 12:05:12 +0530

    error message something wrong to error message

--- middleware/error.js
+++ middleware/error.js
@@ -3,5 +3,5 @@ const winston = require("winston");
 module.exports = function (err, req, res, next) {
   winston.error(err.message, err);
 
-  res.status(500).send("somthing worng");
+  res.status(500).send(err.message);
 };
