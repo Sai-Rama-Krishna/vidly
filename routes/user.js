2dce2648d93d2df4ea35264eff7f9face97e5af7
sai rama krishna <sairamakrishna2@gmail.com>
2021-10-18 16:40:46 +0530

    aaaa

--- routes/user.js
+++ routes/user.js
@@ -8,8 +8,8 @@ const mongoose = require("mongoose");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  // const { error } = validate(req.body);
+  // if (error) return res.status(400).send(error.details[0].message);
 
   let user = await users.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registerd");
