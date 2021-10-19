3c88b287360bda681f8419478a90a546b81c8d70
sai rama krishna <sairamakrishna2@gmail.com>
2021-10-19 09:03:28 +0530

    jjjdd

--- startup/prod.js
+++ startup/prod.js
@@ -5,13 +5,13 @@ const cors = require("cors");
 module.exports = function (app) {
   app.use(helmet());
   app.use(compression());
-  // app.use(
-  //   cors({
-  //     origin: "*",
-  //   })
-  // );
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-  });
+  app.use(
+    cors({
+      origin: "*",
+    })
+  );
+  // app.use((req, res, next) => {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   next();
+  // });
 };
