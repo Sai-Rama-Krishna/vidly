cad1c391ec7a81e359e65dc5927a447e5aaf8321
sai rama krishna <sairamakrishna2@gmail.com>
2021-10-19 08:55:34 +0530

    hhhh

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const config = require("config");
 const app = express();
 
 require("./startup/db")();
+require("./startup/prod")(app);
 require("./startup/routes")(app);
 require("./startup/logging")();
 require("./startup/config")();
 require("./startup/validation")();
-require("./startup/prod")(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => winston.info(`Listening on port ${port}...`));
