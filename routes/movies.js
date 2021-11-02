7347cd028d7609ac8c0fe80ccd6db5bfe2de4f1c
SaiRamaKrishna <sairamakrishna2@gmail.com>
2021-11-02 21:01:30 +0530

    movie put change

--- routes/movies.js
+++ routes/movies.js
@@ -48,35 +48,62 @@ router.post("/", async (req, res) => {
   }, 4000);
 });
 
-// put
 router.put("/:id", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const genre = await Genres.findById(req.body.genreId);
-  if (!genre) return res.status(400).send("invalid genre");
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send("Invalid genre.");
 
-  obj1 = {
-    title: req.body.title,
-    genre: {
-      _id: genre._id,
-      name: genre.name,
+  const movie = await Movie.findByIdAndUpdate(
+    req.params.id,
+    {
+      title: req.body.title,
+      genre: {
+        _id: genre._id,
+        name: genre.name,
+      },
+      numberInStock: req.body.numberInStock,
+      dailyRentalRate: req.body.dailyRentalRate,
     },
-    numberInStock: req.body.numberInStock,
-    dailyRentalRate: req.body.dailyRentalRate,
-  };
+    { new: true }
+  );
 
-  try {
-    const movie = await Movies.findByIdAndUpdate(req.params.id, {
-      obj1,
-    });
-    // console.log( " put" , obj1)
-    if (!movie) return res.status(404).send("not avalabile");
-    await res.send(movie);
-  } catch (err) {
-    console.log(err);
-  }
+  if (!movie)
+    return res.status(404).send("The movie with the given ID was not found.");
+
+  res.send(movie);
 });
+
+// put
+// router.put("/:id", async (req, res) => {
+//   const { error } = validate(req.body);
+//   if (error) return res.status(400).send(error.details[0].message);
+
+//   const genre = await Genres.findById(req.body.genreId);
+//   if (!genre) return res.status(400).send("invalid genre");
+
+//   obj1 = {
+//     title: req.body.title,
+//     genre: {
+//       _id: genre._id,
+//       name: genre.name,
+//     },
+//     numberInStock: req.body.numberInStock,
+//     dailyRentalRate: req.body.dailyRentalRate,
+//   };
+
+//   try {
+//     const movie = await Movies.findByIdAndUpdate(req.params.id, {
+//       obj1,
+//     });
+//     // console.log( " put" , obj1)
+//     if (!movie) return res.status(404).send("not avalabile");
+//     await res.send(movie);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
 router.delete("/:id", async (req, res) => {
   // check id
