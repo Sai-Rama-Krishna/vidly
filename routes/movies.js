a53d8307fd7813063f502537bee7969a43483e81
SaiRamaKrishna <sairamakrishna2@gmail.com>
2021-11-02 18:33:32 +0530

    async add

--- routes/movies.js
+++ routes/movies.js
@@ -22,7 +22,7 @@ router.get("/:id", async (req, res) => {
 //post
 
 router.post("/", async (req, res) => {
-  setTimeout(() => {
+  setTimeout(async () => {
     try {
       const { error } = validate(req.body);
       if (error) return res.status(400).send(error.details[0].message);
