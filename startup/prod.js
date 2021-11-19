6173f855f9a20b0c2caa9cfd3c1ce231b4215538
SaiRamaKrishna <sairamakrishna2@gmail.com>
2021-11-19 15:29:32 +0530

    all

--- startup/prod.js
+++ startup/prod.js
@@ -8,9 +8,10 @@ module.exports = function (app) {
   app.use(
     cors({
       origin: "*",
-      // "Access-Control-Allow-Orgin":"*",
-      // "Acess-Control-Allow-Methods":"GET,POST,DELETE,OPTIONS",
-      // "Acess-Control-Allow-Headers":"Content-Type,Access-Control-Allow-Headers,x-test"
+      "Access-Control-Allow-Orgin": "*",
+      "Acess-Control-Allow-Methods": "GET,POST,DELETE,PUT,OPTIONS",
+      "Acess-Control-Allow-Headers":
+        "Content-Type,Access-Control-Allow-Headers,x-test",
     })
   );
   // app.use((req, res, next) => {
