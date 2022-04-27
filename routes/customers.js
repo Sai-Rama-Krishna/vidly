eeddf1283389703d7d91bbeec8e4e2ef360776f2
Sai-Rama-Krishna <82598484+Sai-Rama-Krishna@users.noreply.github.com>
2022-04-27 13:47:47 +0530

    qd

--- routes/customers.js
+++ routes/customers.js
@@ -64,7 +64,6 @@ catch (err) {
 
 router.put('/:id', async (req , res ) => {
     
-
     const { error} = validate(req.body)
     if(error) return res.status(400).send(error.details[0].message)
     
