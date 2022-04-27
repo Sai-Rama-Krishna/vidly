8f01d628b75b49c6e4eaa081378f905b52caa1f1
Sai-Rama-Krishna <82598484+Sai-Rama-Krishna@users.noreply.github.com>
2022-04-27 13:44:11 +0530

    sasas

--- routes/customers.js
+++ routes/customers.js
@@ -25,7 +25,6 @@ router.get('/',  async ( req, res) => {
     }
 });
 router.post('/', async (req , res ) => { 
-
     try{ 
         const { error} = validate(req.body)
         if(error) return res.status(400).send(error.details[0].message)
