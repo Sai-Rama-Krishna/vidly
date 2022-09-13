8274819648f2a3a38e3118ec6dfa57ffc8b26d7a
Sai-Rama-Krishna <82598484+Sai-Rama-Krishna@users.noreply.github.com>
2022-09-13 19:32:57 +0530

    cjskn

--- startup/routes.js
+++ startup/routes.js
@@ -9,10 +9,10 @@ const express = require("express");
 
 
 module.exports = function (app) {
-const router = express.Router();
-  router.get("/", async (req, res) => {
-  return res.render("App Running");
-});
+
+  app.get('/', async (req, res) => {
+    return res.status(200).send('Hello Welcome to Vidly 🚀 ');
+  });
 
   app.use(express.json());
   app.use("/api/genres", genres);
